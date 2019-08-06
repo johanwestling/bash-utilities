836728ebf0ade64b10848ec48e7edd8281c96dc4
Johan Westling <johan@westling.ax>
2019-08-06 19:07:23 +0300

    Added line break to usage code

--- utilities.sh
+++ utilities.sh
@@ -2,8 +2,11 @@
 source "${bash_root:-.}/utilities/output.sh"
 source "${bash_root:-.}/utilities/input.sh"
 
-select_value=$(bash_select "Select label:" \
-  $(ls -d "${bash_root:-.}/utilities/"*))
+select_value=$(\
+  bash_select \
+    "Select label:" \
+    $(ls -d "${bash_root:-.}/utilities/"*) \
+)
 
 echo ""
 echo "Select value: $select_value"
