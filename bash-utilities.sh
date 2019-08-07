1107abf07ad04b17db07a858385510e0ec1562ac
Johan Westling <johan@westling.ax>
2019-08-07 08:31:39 +0300

    Added examples file

---
+++ bash-utilities.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source "${bash_root:-.}/utilities/output.sh"
+source "${bash_root:-.}/utilities/input.sh"
--- examples.sh
+++ examples.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-source "${bash_root:-.}/utilities/output.sh"
-source "${bash_root:-.}/utilities/input.sh"
+source "bash-utilities.sh"
 
 select_options=$(ls -d "${bash_root:-.}/utilities/"*)
 select_value=$(bash_select "Select label:" ${select_options})
@@ -10,3 +9,5 @@ input_value=$(bash_input "Input label:")
 bash_label "Form values:"
 echo "select_value=${select_value}" | bash_indent
 echo "input_value=${input_value}" | bash_indent
+
+bash_newline
